453aad3070b69bb477f7a8b6b27641991528ada1
Joe Seymour <joe.seymour@carefusion.com>
2016-05-23 15:01:10 -0700

    two zones working with attributes

--- src/smartthings_relay.groovy
+++ src/smartthings_relay.groovy
@@ -1,32 +1,42 @@
 metadata {
-	definition (name: "Relay Switches", namespace: "smartthings", author: "poindexter12") {
+	definition (name: "8 Channel Relay", namespace: "poindexter12", author: "Joe Seymour") {
 		capability "Switch"
 		capability "Actuator"
 		capability "Sensor"
+		attribute "zone1", "string"
+		attribute "zone2", "string"
+        command "zone1on"
+        command "zone1off"
+        command "zone2on"
+        command "zone2off"
 	}
 
 	// simulator metadata
 	simulator {
-		// status messages
-		status "on": "on/off: 1"
-		status "off": "on/off: 0"
+		status "on":  "command: 2003, payload: FF"
+		status "off": "command: 2003, payload: 00"
 
-		// reply messages
-		reply "zcl on-off on": "on/off: 1"
-		reply "zcl on-off off": "on/off: 0"
+		reply "2001FF,delay 100,2502": "command: 2503, payload: FF"
+		reply "200100,delay 100,2502": "command: 2503, payload: 00"
 	}
 
 	tiles(scale: 2) {
 		multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
 			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
-				attributeState "on", label:'${name}', action:"switch.off", icon:"st.switches.switch.on", backgroundColor:"#79b821", nextState:"turningOff"
-				attributeState "off", label:'${name}', action:"switch.on", icon:"st.switches.switch.off", backgroundColor:"#ffffff", nextState:"turningOn"
-				attributeState "turningOn", label:'${name}', action:"switch.off", icon:"st.switches.switch.on", backgroundColor:"#79b821", nextState:"turningOff"
-				attributeState "turningOff", label:'${name}', action:"switch.on", icon:"st.switches.switch.off", backgroundColor:"#ffffff", nextState:"turningOn"
+				attributeState "on", label:'${name}', action:"switch.off", icon:"st.switches.switch.on", backgroundColor:"#79b821"
+				attributeState "off", label:'${name}', action:"switch.on", icon:"st.switches.switch.off", backgroundColor:"#ffffff"
 			}
 		}
+    standardTile("zone1", "device.zone1", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+      state "on", label:'${name}', action:"zone1off", icon:"st.switches.switch.on", backgroundColor:"#79b821"
+      state "off", label:'${name}', action:"zone1on", icon:"st.switches.switch.off", backgroundColor:"#ffffff"
+		}
+    standardTile("zone2", "device.zone2", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+      state "on", label:'${name}', action:"zone2off", icon:"st.switches.switch.on", backgroundColor:"#79b821"
+      state "off", label:'${name}', action:"zone2off", icon:"st.switches.switch.off", backgroundColor:"#ffffff"
+		}
 		main "switch"
-		details(["switch"])
+		details(["switch", "zone1", "zone2"])
 	}
 }
 
@@ -36,13 +46,38 @@ def parse(String description) {
 }
 
 def off() {
-	relayCommand("1", "0")
+	def textCommand = "1,0|2,0"
+	log.debug "sending command ${textCommand}"
+	zigbee.smartShield(text: textCommand)
+	sendEvent(name: "switch", value: "off")
 }
 
 def on() {
-	relayCommand("1", "1")
+	def textCommand = "1,1|2,1"
+	log.debug "sending command ${textCommand}"
+	zigbee.smartShield(text: textCommand)
+	sendEvent(name: "switch", value: "on")
 }
 
-relayCommand(relayNumber, attribute){
-  zigbee.smartShield(text: "${relayNumber},${attribute}").format()
+def zone1on(){
+  relayCommand(1, 1)
+}
+
+def zone1off(){
+  relayCommand(1, 0)
+}
+
+def zone2on(){
+  relayCommand(2, 1)
+}
+
+def zone2off(){
+  relayCommand(2, 0)
+}
+
+def relayCommand(relayNumber, attribute){
+	def textCommand = "${relayNumber},${attribute}"
+	log.debug "sending command ${textCommand}"
+	zigbee.smartShield(text: "${textCommand}")
+	sendEvent(name: "zone${relayNumber}", value: attribute == 1 ? "on" : "off")
 }
