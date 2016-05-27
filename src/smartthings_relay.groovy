89426632e2aa75ec376d651f74808111beb1e7ea
Joe Seymour <joe.seymour@carefusion.com>
2016-05-27 13:42:06 -0700

    fixing commands, adding all off

--- src/smartthings_relay.groovy
+++ src/smartthings_relay.groovy
@@ -1,14 +1,13 @@
 metadata {
 	definition (name: "8 Channel Relay", namespace: "poindexter12", author: "Joe Seymour") {
-		capability "Switch"
 		capability "Actuator"
-		capability "Sensor"
 		attribute "zone1", "string"
 		attribute "zone2", "string"
-        command "zone1on"
-        command "zone1off"
-        command "zone2on"
-        command "zone2off"
+        command "alloff"
+		command "zone1on"
+		command "zone1off"
+		command "zone2on"
+		command "zone2off"
 	}
 
 	// simulator metadata
@@ -20,64 +19,48 @@ metadata {
 		reply "200100,delay 100,2502": "command: 2503, payload: 00"
 	}
 
-	tiles(scale: 2) {
-		multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
-			tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
-				attributeState "on", label:'${name}', action:"switch.off", icon:"st.switches.switch.on", backgroundColor:"#79b821"
-				attributeState "off", label:'${name}', action:"switch.on", icon:"st.switches.switch.off", backgroundColor:"#ffffff"
-			}
+	tiles(scale: 1) {
+		standardTile("status", "status", width: 2, height: 1) {
+			state "off", label:'${name}', icon:"st.switches.switch.off", backgroundColor:"#ffffff"
+			state "on", label:'${name}', icon:"st.switches.switch.on", backgroundColor:"#79b821"
 		}
-    standardTile("zone1", "device.zone1", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-      state "on", label:'${name}', action:"zone1off", icon:"st.switches.switch.on", backgroundColor:"#79b821"
-      state "off", label:'${name}', action:"zone1on", icon:"st.switches.switch.off", backgroundColor:"#ffffff"
+        standardTile("alloff", "device.alloff", decoration: "flat") {
+			state "none", label: "all off", action:"alloff", icon:"st.switches.switch.off", backgroundColor:"#ffffff"
+        }
+		standardTile("zone1", "device.zone1", inactiveLabel: false, decoration: "flat") {
+			state "off", label:'${name}', action:"zone1on", icon:"st.switches.switch.off", backgroundColor:"#ffffff", nextState: "on"
+			state "on", label:'${name}', action:"zone1off", icon:"st.switches.switch.on", backgroundColor:"#79b821", nextState: "off"
 		}
-    standardTile("zone2", "device.zone2", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
-      state "on", label:'${name}', action:"zone2off", icon:"st.switches.switch.on", backgroundColor:"#79b821"
-      state "off", label:'${name}', action:"zone2off", icon:"st.switches.switch.off", backgroundColor:"#ffffff"
+		standardTile("zone2", "device.zone2", inactiveLabel: false, decoration: "flat") {
+			state "off", label:'${name}', action:"zone2on", icon:"st.switches.switch.off", backgroundColor:"#ffffff", nextState: "on"
+			state "on", label:'${name}', action:"zone2off", icon:"st.switches.switch.on", backgroundColor:"#79b821", nextState: "off"
 		}
-		main "switch"
-		details(["switch", "zone1", "zone2"])
+		main "status"
+		details(["status", "alloff", "zone1", "zone2"])
 	}
 }
 
 // Parse incoming device messages to generate events
 def parse(String description) {
-  // TBD, if anything
-}
-
-def off() {
-	def textCommand = "1,0|2,0"
-	log.debug "sending command ${textCommand}"
-	zigbee.smartShield(text: textCommand)
-	sendEvent(name: "switch", value: "off")
-}
-
-def on() {
-	def textCommand = "1,1|2,1"
-	log.debug "sending command ${textCommand}"
-	zigbee.smartShield(text: textCommand)
-	sendEvent(name: "switch", value: "on")
+	// TBD, if anything
 }
 
 def zone1on(){
-  relayCommand(1, 1)
+	zigbee.smartShield(text: "1,1").format()
 }
 
 def zone1off(){
-  relayCommand(1, 0)
+	zigbee.smartShield(text: "1,0").format()
 }
 
 def zone2on(){
-  relayCommand(2, 1)
+	zigbee.smartShield(text: "2,1").format()
 }
 
 def zone2off(){
-  relayCommand(2, 0)
+	zigbee.smartShield(text: "2,0").format()
 }
 
-def relayCommand(relayNumber, attribute){
-	def textCommand = "${relayNumber},${attribute}"
-	log.debug "sending command ${textCommand}"
-	zigbee.smartShield(text: "${textCommand}")
-	sendEvent(name: "zone${relayNumber}", value: attribute == 1 ? "on" : "off")
+def alloff(){
+	zigbee.smartShield(text: "1,0|2,0|3,0|4,0|5,0|6,0|7,0|8,0").format()
 }
